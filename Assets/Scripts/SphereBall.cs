a52978ff90239d88be7a6780302d7cfa21006aba
Humberto Trujillo <humberto.trujillo@uabc.edu.mx>
2019-03-22 18:34:47 -0700

    Se añadió práctica de manejo de Inventario.

---
+++ Assets/Scripts/SphereBall.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereBall : Interactable 
+{
+	public float kickForce = 30f;
+	public Vector3 kickDirection;
+	Rigidbody rb;
+
+	/// <summary>
+	/// Awake is called when the script instance is being loaded.
+	/// </summary>
+	void Awake()
+	{
+		rb = GetComponent<Rigidbody>();
+	}
+	public override void Interact()
+	{
+		Debug.Log("SphereBall");
+		rb.AddForce(kickDirection * kickForce, ForceMode.Force);
+	}
+}
